3d351c2c5ff08ed98fd9bebfae3c3d7ab2815f00
Rakhat Zhumabek <rakhat.study@gmail.com>
2025-07-08 10:29:56 +0500

    Init commit. Python Basics

---
+++ data-analysis-roadmap/Python/Basics/hello.py
@@ -0,0 +1,9 @@
+import sys
+
+my_name = "Rakhat"
+my_goal = "Learn Data Analysis in 2 month"
+
+print(f"Hello! My name is {my_name}")
+print(f"My Goal: {my_goal}")
+print("-" * 20)
+print(f"I use Python version of {sys.version}")
